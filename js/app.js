d5bc55736583508ea75008fa4409391555330517
DarkCode <DarkCodea@gmail.com>
2023-03-25 07:21:12 +0300

    Make it responsive

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 // Select Items 
 line = document.getElementById('line');
+barsBtn = document.querySelector('header .nav i')
+navigation = document.querySelector('header .nav ul')
 
 
 // get height of page 
@@ -15,3 +17,26 @@ window.addEventListener('scroll', () => {
 
     line.style.width = `${(scrollTop / height) * 100}%`
 })
+
+
+// Open Navbar When Button is Clicked 
+
+barsBtn.addEventListener('click', () => {
+    navigation.classList.toggle('hide')
+    if(navigation.classList.contains('hide')) {
+        barsBtn.style.transform = 'rotate(360deg)'
+        barsBtn.className = 'fa-solid fa-xmark'
+    }else {
+        barsBtn.style.transform = 'rotate(0deg)'
+        barsBtn.className = 'fa-solid fa-bars-staggered'
+    }
+})
+
+
+
+var typed = new Typed('.element', {
+    strings: ['Frontend Developer.', 'Problem Solver.', 'Obsessed with website design.'],
+    typeSpeed: 100,
+    loop: true,
+    showCursor: false,
+});
